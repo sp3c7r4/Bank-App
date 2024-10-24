84c226a927f9aeefc5b6163d074471ec8fb18cfe
Adedayoke <adedayoke2006@gmail.com>
2024-10-24 09:47:36 +0100

    crypto sell init

--- screens/Dashboard Components/CryptoButtonTile.jsx
+++ screens/Dashboard Components/CryptoButtonTile.jsx
@@ -14,40 +14,70 @@ import { CryptoContext } from "../../context/CryptoContext";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import axios from "axios";
 import useRefetchUser from "../../hooks/useRefetchUser";
+import Slider from "@react-native-community/slider";
 
 export default function CryptoButtonTile({ buttonSwapMode }) {
   const [toggle, setToggle] = useState("swap");
   const { currentUser } = useContext(UserContext);
   const { dispatch, allCryptoCurrencies } = useContext(CryptoContext);
-  const [cryptovalue, setCryptoValue] = useState("0");
+  const [cryptovalue, setCryptoValue] = useState("");
   const [usdvalue, setUsdValue] = useState("");
   const [selectedCrypto, setSelectedCrypto] = useState("");
   const [toCrypto, setToCrypto] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const { refetchUser } = useRefetchUser(setIsLoading);
+  const userCryptoCurrencies = [
+    {
+      symbol: "BTC",
+      amount: currentUser?.cryptos[0].btc_balance,
+    },
+    {
+      symbol: "USDT",
+      amount: currentUser?.cryptos[0].usdt_balance,
+    },
+    {
+      symbol: "BNB",
+      amount: currentUser?.cryptos[0].bnb_balance,
+    },
+    {
+      symbol: "ETH",
+      amount: currentUser?.cryptos[0].eth_balance,
+    },
+  ];
 
   // console.log(buttonSwapMode)
   useEffect(function () {
     dispatch({ type: "setCryptoInView", payload: true });
   }, []);
   useEffect(() => {
-    if (selectedCrypto) {
-      const equUsd = allCryptoCurrencies.find(
-        (crp) => crp?.symbol === selectedCrypto
-      );
-      if (selectedCrypto && equUsd?.priceUsd && !isNaN(usdvalue)) {
-        setCryptoValue(+usdvalue / +equUsd?.priceUsd);
-        console.log(usdvalue, cryptovalue);
-      } else {
-        setCryptoValue("");
-      }
-    }
-  }, [usdvalue, selectedCrypto, allCryptoCurrencies]);
-  useEffect(() => {
     if (buttonSwapMode !== undefined) {
       setToggle(buttonSwapMode);
     }
   }, [buttonSwapMode]);
+
+  useEffect(() => {
+    const equUsd = allCryptoCurrencies.find(
+      (crp) => crp?.symbol === selectedCrypto
+    );
+    if (selectedCrypto && equUsd.priceUsd && !isNaN(usdvalue)) {
+      setCryptoValue(+usdvalue / +equUsd.priceUsd);
+    } else {
+      setCryptoValue("");
+    }
+  }, [usdvalue, selectedCrypto]);
+  useEffect(
+    function () {
+      const equUsd = allCryptoCurrencies.find(
+        (crp) => crp?.symbol === selectedCrypto
+      );
+      if (selectedCrypto && equUsd.priceUsd && !isNaN(cryptovalue)) {
+        setUsdValue(+cryptovalue * +equUsd.priceUsd);
+      } else {
+        setUsdValue("");
+      }
+    },
+    [cryptovalue, selectedCrypto]
+  );
 
   const switchToggle = (mode) => {
     mode === "swap" ? setToggle("swap") : setToggle("P2P");
@@ -72,28 +102,15 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
       value: "USDT",
     },
   ];
-  const handleUSDValue = (valueRecieved) => {
-    const value = parseFloat(valueRecieved);
-    if (!isNaN(value)) {
-      setUsdValue(value.toString());
-      if (value > currentUser.balance) {
-        // setError("Insufficient balance");
-      } else {
-        // setError("");
-      }
-    } else {
-      setUsdValue("");
-    }
-  };
   const handleSubmit = async () => {
     // setIsLoadingCrypto(true);
-    console.log(selectedCrypto)
+    console.log(selectedCrypto);
     if (!selectedCrypto) {
       // setIsLoadingCrypto(false);
       // toast.error("Please select a cryptocurrency.");
       return;
     }
-  
+
     const handleTransactionSuccess = () => {
       // setIsLoadingCrypto(false);
       // toast.success("Transaction Successful");
@@ -102,19 +119,19 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
       // localStorage.removeItem("transactions");
       // localStorage.removeItem("transactions_timestamp");
     };
-  
+
     const handleTransactionError = (message) => {
       // setIsLoadingCrypto(false);
       // toast.error(message || "Error making transaction");
       // setCryptoPage(false);
       refetchUser();
     };
-  
+
     const transactionType = toCrypto ? "buy" : "sell";
     const value = toCrypto ? +usdvalue : +cryptovalue;
-  
+
     try {
-      console.log("submitting")
+      console.log("submitting");
       const response = await axios.post(
         `https://api.montrealtriustfinancial.online/currency/crypto/${transactionType}`,
         {
@@ -123,19 +140,20 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
           asset: selectedCrypto,
         }
       );
-  
+
       if (response.data.status === "success") {
-        console.log("success")
+        console.log("success");
         handleTransactionSuccess();
       } else {
         handleTransactionError(response.data.message || "Transaction failed");
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
       // Handle network or server-side errors
       if (err.response) {
         // Server responded with a status other than 200
-        const serverMessage = err.response.data?.message || "Transaction failed.";
+        const serverMessage =
+          err.response.data?.message || "Transaction failed.";
         handleTransactionError(serverMessage);
       } else if (err.request) {
         // Request was made but no response was received
@@ -146,6 +164,9 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
       }
     }
   };
+  // function handleSliderChange(value) {
+  //   setCryptoValue(value / 100000);
+  // }
   return (
     <View>
       {/*Toggle*/}
@@ -206,69 +227,147 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
                   <Text>Send</Text>
                   <Text>Balance: ${currentUser?.balance.toFixed(2)}</Text>
                 </View>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    marginVertical: 20,
-                    borderRadius: 10,
-                  }}
-                >
-                  <View
-                    style={{
-                      alignItems: "center",
-                      justifyContent: "center",
-                      marginRight: 30,
-                      flexBasis: "15%",
-                    }}
-                  >
-                    <Text
+                {toCrypto ? (
+                  <>
+                    <View
                       style={{
-                        fontFamily: "outfit-bold",
-                        // color: "#f8f8fb",
-                        fontSize: 20,
+                        flexDirection: "row",
+                        marginVertical: 20,
+                        borderRadius: 10,
                       }}
                     >
-                      USD
-                    </Text>
-                  </View>
+                      <View
+                        style={{
+                          alignItems: "center",
+                          justifyContent: "center",
+                          marginRight: 30,
+                          flexBasis: "15%",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontFamily: "outfit-bold",
+                            // color: "#f8f8fb",
+                            fontSize: 20,
+                          }}
+                        >
+                          USD
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          // width: "100%",
+                          flexBasis: "75%",
+                          height: 50,
+                          borderRadius: 10,
+                          // position: "absolute",
+                          zIndex: 1,
+                          backgroundColor: "#f8f8fb",
+                          justifyContent: "center",
+                          // paddingLeft: 120,
+                        }}
+                      >
+                        <TextInput
+                          placeholder="0.00"
+                          keyboardType="numeric"
+                          value={usdvalue==="0"?"":usdvalue}
+                          onChangeText={(value)=>setUsdValue(value)}
+                          style={{
+                            fontFamily: "outfit-bold",
+                            textAlign: "right",
+                            color: "#000",
+                            opacity: 0.5,
+                            fontSize: 20,
+                            width: "100%",
+                            // backgroundColor: "red"
+                          }}
+                        />
+                      </View>
+                    </View>
+                  </>
+                ) : (
                   <View
                     style={{
-                      // width: "100%",
-                      flexBasis: "75%",
-                      height: 50,
+                      flexDirection: "row",
+                      marginVertical: 20,
                       borderRadius: 10,
-                      // position: "absolute",
-                      zIndex: 1,
-                      backgroundColor: "#f8f8fb",
-                      justifyContent: "center",
-                      // paddingLeft: 120,
                     }}
                   >
-                    <TextInput
-                      placeholder="0.00"
-                      keyboardType="numeric"
-                      value={usdvalue}
-                      onChangeText={(value) => handleUSDValue(value)}
+                    <View
                       style={{
-                        fontFamily: "outfit-bold",
-                        textAlign: "right",
-                        color: "#000",
-                        opacity: 0.5,
-                        fontSize: 20,
-                        width: "100%",
-                        // backgroundColor: "red"
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginRight: 30,
+                        flexBasis: "50%",
                       }}
-                    />
+                    >
+                      <RNPickerSelect
+                        placeholder={{ label: "Select a crypto currency" }}
+                        onValueChange={(value) => setSelectedCrypto(value)}
+                        items={cryptoCurrencies}
+                        style={{
+                          inputAndroid: {
+                            fontFamily: "outfit-bold", // for Android
+                            fontSize: 8,
+                            borderRadius: 10,
+                          },
+                        }}
+                      />
+                    </View>
+
+                    <View
+                      style={{
+                        flexBasis: "40%",
+                        height: 50,
+                        borderRadius: 10,
+                        backgroundColor: "#f8f8fb",
+                        justifyContent: "center",
+                      }}
+                    >
+                    <TextInput
+                        placeholder="0.0000"
+                        value={cryptovalue==="0" ? "" : cryptovalue}
+                        onChangeText={(value)=>setCryptoValue(value)}
+                        style={{
+                          fontFamily: "outfit-bold",
+                          textAlign: "right",
+                          color: "#000",
+                          opacity: 0.5,
+                          fontSize: 13,
+                          width: "100%",
+                        }}
+                      />
+                      {/* <Slider
+                        style={{ width: 200, height: 40 }}
+                        minimumValue={1} // We set this to 1 to avoid precision errors
+                        maximumValue={
+                          userCryptoCurrencies.find(
+                            (crypto) => crypto?.symbol === selectedCrypto
+                          )?.amount * 100000
+                        } // Scaling to 100000
+                        step={1} // This gives us precision control over the slider
+                        value={Number(cryptovalue) * 100000} // Adjust the scale of cryptovalue to match the slider
+                        onValueChange={(value) =>
+                          setCryptoValue((value / 100000).toFixed(5))
+                        } // Convert slider value to crypto value, with precision up to 5 decimal places
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#b9e4c9"
+                      /> */}
+                    </View>
                   </View>
-                </View>
+                )}
               </View>
-              <View style={{ alignItems: "flex-end", marginVertical: 10 }}>
+              <TouchableOpacity
+                onPress={() => setToCrypto((cp) => !cp)}
+                style={{ alignItems: "flex-end", marginVertical: 10 }}
+              >
                 <Image
                   style={{ width: 45, height: 45 }}
                   resizeMode="contain"
                   source={require("./../../assets/images/swap_crypto_black.png")}
                 />
-              </View>
+              </TouchableOpacity>
               <View
                 style={{
                   backgroundColor: "#f8f8fb",
@@ -276,58 +375,118 @@ export default function CryptoButtonTile({ buttonSwapMode }) {
                   borderRadius: 10,
                 }}
               >
-                <View
-                  style={{
-                    flexDirection: "row",
-                    marginVertical: 20,
-                    borderRadius: 10,
-                  }}
-                >
+                {toCrypto ? (
                   <View
                     style={{
-                      alignItems: "center",
-                      justifyContent: "center",
-                      marginRight: 30,
-                      flexBasis: "50%",
-                    }}
-                  >
-                    <RNPickerSelect
-                      placeholder={{ label: "Select a crypto currency" }}
-                      onValueChange={(value) => setSelectedCrypto(value)}
-                      items={cryptoCurrencies}
-                      style={{
-                        inputAndroid: {
-                          fontFamily: "outfit-bold", // for Android
-                          fontSize: 8,
-                          borderRadius: 10,
-                        },
-                      }}
-                    />
-                  </View>
-
-                  <View
-                    style={{
-                      flexBasis: "40%",
-                      height: 50,
+                      flexDirection: "row",
+                      marginVertical: 20,
                       borderRadius: 10,
-                      backgroundColor: "#f8f8fb",
-                      justifyContent: "center",
                     }}
                   >
-                    <TextInput
-                      placeholder="0.00"
-                      value={Number(cryptovalue)?.toFixed(4)}
+                    <View
                       style={{
-                        fontFamily: "outfit-bold",
-                        textAlign: "right",
-                        color: "#000",
-                        opacity: 0.5,
-                        fontSize: 13,
-                        width: "100%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginRight: 30,
+                        flexBasis: "50%",
                       }}
-                    />
+                    >
+                      <RNPickerSelect
+                        placeholder={{ label: "Select a crypto currency" }}
+                        onValueChange={(value) => setSelectedCrypto(value)}
+                        items={cryptoCurrencies}
+                        style={{
+                          inputAndroid: {
+                            fontFamily: "outfit-bold", // for Android
+                            fontSize: 8,
+                            borderRadius: 10,
+                          },
+                        }}
+                      />
+                    </View>
+
+                    <View
+                      style={{
+                        flexBasis: "40%",
+                        height: 50,
+                        borderRadius: 10,
+                        backgroundColor: "#f8f8fb",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <TextInput
+                        placeholder="0.00"
+                        value={Number(cryptovalue)?.toFixed(4)}
+                        style={{
+                          fontFamily: "outfit-bold",
+                          textAlign: "right",
+                          color: "#000",
+                          opacity: 0.5,
+                          fontSize: 13,
+                          width: "100%",
+                        }}
+                      />
+                    </View>
                   </View>
-                </View>
+                ) : (
+                  <>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        marginVertical: 20,
+                        borderRadius: 10,
+                      }}
+                    >
+                      <View
+                        style={{
+                          alignItems: "center",
+                          justifyContent: "center",
+                          marginRight: 30,
+                          flexBasis: "15%",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontFamily: "outfit-bold",
+                            // color: "#f8f8fb",
+                            fontSize: 20,
+                          }}
+                        >
+                          USD
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          // width: "100%",
+                          flexBasis: "75%",
+                          height: 50,
+                          borderRadius: 10,
+                          // position: "absolute",
+                          zIndex: 1,
+                          backgroundColor: "#f8f8fb",
+                          justifyContent: "center",
+                          // paddingLeft: 120,
+                        }}
+                      >
+                        <TextInput
+                          placeholder="0.00"
+                          keyboardType="numeric"
+                          value={usdvalue==="0"?"":usdvalue}
+                          // onChangeText={(value) => handleUSDValue(value)}
+                          style={{
+                            fontFamily: "outfit-bold",
+                            textAlign: "right",
+                            color: "#000",
+                            opacity: 0.5,
+                            fontSize: 20,
+                            width: "100%",
+                            // backgroundColor: "red"
+                          }}
+                        />
+                      </View>
+                    </View>
+                  </>
+                )}
               </View>
               <TouchableOpacity
                 onPress={handleSubmit}
